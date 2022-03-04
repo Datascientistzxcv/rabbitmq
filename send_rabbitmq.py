d6b74e0dbe414932ef75517ed77a731b40629d03
Datascientistzxcv <shekharsuman0747@gmail.com>
2022-03-04 13:37:57 +0530

    rabbitmq

---
+++ send_rabbitmq.py
@@ -0,0 +1,13 @@
+import pika
+import json
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+with open("/home/flyingspark/Downloads/freelnacer-linkedin/linkedin", 'r') as myfile:
+    data=myfile.read()
+data=json.loads(data) 
+channel.basic_publish(exchange='', routing_key='hello', body=json.dumps(data))
+print("send data")
+connection.close()
